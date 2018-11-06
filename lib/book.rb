4cad2c19edd1e89cb162e8cd967c44b766a26524
Paula McKinley <pmknyc@optimum.net>
2018-11-06 18:10:06 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,9 +1,17 @@
 class Book
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
+  attr_accessor :author, :page_count
+  attr_reader :title, :genre #moved genre, make reader only
+  GENRES = []
+
 
   def initialize(title)
     @title = title
+  end
+
+  # create Writer for genre to incorporate GENRES class constant
+  def genre=(genre)
+    @genre = genre
+    GENRES << genre
   end
 
   def turn_page
